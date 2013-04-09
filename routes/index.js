490e710635a4ae56d5a3377cb19436c916907760
Kevin Lamping <klamping@gmail.com>
2013-04-09 11:45:13 -0500

    fixed index issue

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@
 /*
  * GET home page.
  */
+var days = [
+   { title: 'Day One - First Steps' },
+   { title: 'Day Two - Testing Trim' },
+   { title: 'Day Three' },
+   { title: 'Day Four' },
+   { title: 'Day Five' },
+   { title: 'Day Six' },
+   { title: 'Day Seven' },
+   { title: 'Day Eight' },
+   { title: 'Day Nine' },
+   { title: 'Day Ten' }
+];
 
 exports.index = function(req, res){
-  res.render('day-1', { title: 'Test-Driven JavaScript' });
+  res.render('index', { title: 'Test-Driven JavaScript', days: days, activeDay: 0 });
 };
 
