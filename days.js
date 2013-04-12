2421468307ebe8339d3fa20c70bfb1e3a35d0333
Kevin Lamping <klamping@gmail.com>
2013-04-12 00:31:39 -0500

    day 5

--- days.js
+++ days.js
@@ -7,11 +7,15 @@ var days = [{
 }, {
    title: 'Day Four - BDD & Jasmine' //
 }, {
-   title: 'Day Five' // Asynchronous code/Working with the dom (Mock Objects)/Simulated Event
+   title: 'Day Five - Working with the dom'
 }, {
-   title: 'Day Six'  // Functional testing - PhantomJS & CasperJS
+   title: 'Day Six'  // Functional testing, mock objects - PhantomJS & CasperJS
 }, {
-   title: 'Day Seven' // Why this is hard... Writing Testable JavaScript
+   title: 'Day Seven' // Why this is hard... 
+   // http://www.adequatelygood.com/Writing-Testable-JavaScript.html
+   // https://shanetomlinson.com/2013/testing-javascript-frontend-part-1-anti-patterns-and-fixes/
+   // http://www.nczonline.net/blog/2009/11/17/the-curious-case-of-javascript-unit-testing/
+   // http://www.youtube.com/watch?v=OzjogCFO4Zo
 }, {
    title: 'Day Eight' // YETI, Common patterns
 }, {
