065b00630fb5bfe6f5241916a346ee82c84e6714
Kevin Lamping <klamping@gmail.com>
2013-04-13 15:39:12 -0500

    re-organized page templates

--- days.js
+++ days.js
@@ -9,19 +9,15 @@ var days = [{
 }, {
    title: 'Day Five - Working with the DOM'
 }, {
-   title: 'Day Six'  // Functional testing, mock objects - PhantomJS & CasperJS
+   title: 'Day Six - Mock objects & Spies'  // + jsdom and YUI
 }, {
-   title: 'Day Seven' // Why this is hard...
-   // http://www.adequatelygood.com/Writing-Testable-JavaScript.html
-   // https://shanetomlinson.com/2013/testing-javascript-frontend-part-1-anti-patterns-and-fixes/
-   // http://www.nczonline.net/blog/2009/11/17/the-curious-case-of-javascript-unit-testing/
-   // http://www.youtube.com/watch?v=OzjogCFO4Zo
+   title: 'Day Seven - Structure, YETI'
 }, {
-   title: 'Day Eight' // YETI, Common patterns
+   title: 'Day Eight - Why this is hard'
 }, {
-   title: 'Day Nine' // Toggle - Day One (Defining Requirements, Unit tests)
+   title: 'Day Nine - Final Project - Part 1'
 }, {
-   title: 'Day Ten' // Toggle Day Two (Functional Tests)
+   title: 'Day Ten - Final Project - Part 2'
 }];
 
 exports.days = days;
